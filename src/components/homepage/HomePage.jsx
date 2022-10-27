aeef677d7cbcd2b5afc5ce7569300bf2ded852ae
Bunmi Oye <bunmioye09@gmail.com>
2022-10-27 05:30:34 +0100

    create homepage

---
+++ src/components/homepage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Wrap, Section } from './home-page.style';
+
+const HomePage = () => {
+  return (
+    <Container>
+      <Wrap>
+        <Section></Section>
+      </Wrap>
+    </Container>
+  );
+};
+
+export default HomePage;
---
+++ src/components/homepage/home-page.style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const Wrap = styled.div`
+  display: flex;
+
+  margin: 0 auto;
+  padding: 24px;
+  max-width: 1200px;
+
+  border: 3px solid white; // TODO delete
+`;
+
+export const Section = styled.div`
+  border: 3px solid red; // TODO delete
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+`;
+
+// export const Section = styled.div``;
