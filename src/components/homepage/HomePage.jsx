ca766606d16576200ecbcddadd493770a826a2bf
Bunmi Oye <bunmioye09@gmail.com>
2022-10-28 23:39:08 +0100

    add link buttons

--- src/components/homepage/HomePage.jsx
+++ src/components/homepage/HomePage.jsx
@@ -6,11 +6,27 @@ import {
   ProfilePhotoWrap,
   ProfilePhoto,
   Name,
+  Link,
+  Text,
 } from './home-page.style';
 
 import profile from '../../assets/images/profile.jpg';
 
 const HomePage = () => {
+  const Links = ({ links }) => {
+    return (
+      <>
+        {links.map(({ value, url, resetCase }, i) => {
+          return (
+            <Link href={url} key={url + i}>
+              <Text textTrans={resetCase ? 'unset' : ''}>{value}</Text>
+            </Link>
+          );
+        })}
+      </>
+    );
+  };
+
   return (
     <Container>
       <Wrap>
@@ -19,6 +35,22 @@ const HomePage = () => {
             <ProfilePhoto src={profile} alt="" />
           </ProfilePhotoWrap>
           <Name>Bunmi Oye</Name>
+        </Section>
+        <Section>
+          <Links
+            links={[
+              { value: 'twitter link', url: 'https://twitter.com' },
+              { value: 'zuri team', url: '/' },
+              { value: 'zuri books', url: '/' },
+              { value: 'python books', url: '/' },
+              {
+                value: 'Background Check for Coders',
+                url: '/',
+                resetCase: true,
+              },
+              { value: 'design books', url: '/' },
+            ]}
+          />
         </Section>
       </Wrap>
     </Container>
--- src/components/homepage/home-page.style.js
+++ src/components/homepage/home-page.style.js
@@ -6,10 +6,13 @@ export const Container = styled.div`
 
 export const Wrap = styled.div`
   display: flex;
+  flex-direction: column;
 
   margin: 0 auto;
-  padding: 24px;
+  padding: 64px 12px;
   max-width: 1200px;
+
+  border: 3px solid black; // TODO delete
 `;
 
 export const Section = styled.div`
@@ -17,10 +20,20 @@ export const Section = styled.div`
   flex-direction: column;
   align-items: center;
 
+  margin-bottom: 56px;
   width: 100%;
+  padding: ${props => props.pad || ''};
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+
+  /* border: 3px solid red; // TODO delete */
 `;
 
-export const ProfilePhotoWrap = styled.div``;
+export const ProfilePhotoWrap = styled.div`
+  margin-bottom: 24px;
+`;
 
 export const ProfilePhoto = styled.img`
   width: 88px;
@@ -29,12 +42,40 @@ export const ProfilePhoto = styled.img`
 `;
 
 export const Name = styled.h1`
-  /* font-family: Inter; */
   font-size: 20px;
   font-weight: 700;
   line-height: 30px;
   letter-spacing: 0em;
   text-align: center;
+`;
+
+export const Link = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  margin-bottom: 24px;
+  width: 100%;
+  height: 76px;
+  /* text-align: center; */
+  border-radius: 8px;
+
+  background-color: #eaecf0;
+  color: inherit;
+  text-decoration: none;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 28px;
+  letter-spacing: 0em;
+  text-align: center;
+  text-transform: ${props => props.textTrans || 'capitalize'};
 `;
 
 // export const Section = styled.div``;
