e31b7ee1b641d427bbe88bb75bc76c54dd00ad5e
Bunmi Oye <bunmioye09@gmail.com>
2022-11-01 09:12:42 +0100

    Add all files (to fix build issue)

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import GlobalStyles from './global.style';
+import HomePage from './components/HomePage';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,139 @@
+import React from 'react';
+import {
+  Container,
+  Wrap,
+  Section,
+  PhotoWrap,
+  ProfilePhoto,
+  Name,
+  Link,
+  ButtonLink,
+  Text,
+  Photo,
+} from './home-page.style';
+
+import profile from '../../assets/images/profile.jpg';
+import slack from '../../assets/images/slack.svg';
+import github from '../../assets/images/github.svg';
+import zuri from '../../assets/images/Zuri.Internship_Logo.svg';
+import i4g from '../../assets/images/I4G.svg';
+
+const HomePage = () => {
+  const Links = ({ links }) => {
+    return (
+      <>
+        {links.map(({ id, value, subtext, url, resetCase }, i) => {
+          return (
+            <ButtonLink
+              id={id}
+              href={url}
+              key={url + i}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Text textTrans={resetCase ? 'unset' : ''}>
+                {value}
+                {subtext ? `: ${subtext}` : ''}
+              </Text>
+            </ButtonLink>
+          );
+        })}
+      </>
+    );
+  };
+
+  return (
+    <Container>
+      <Wrap>
+        {/* PROFILE SECTION */}
+        <Section>
+          <PhotoWrap mar="0 0 24px 0">
+            <ProfilePhoto src={profile} alt="" id="profile__img" />
+          </PhotoWrap>
+          <Name id="twitter">Cyanhead</Name>
+          <Name id="slack" style={{ display: 'none' }}>
+            Cyanhead
+          </Name>
+        </Section>
+
+        {/* LINKS SECTION */}
+        <Section mar="0 0 24px 0">
+          <Links
+            links={[
+              {
+                id: 'twitter__link',
+                value: 'Twitter link',
+                url: 'https://twitter.com/cyanhead',
+              },
+              {
+                id: 'btn__zuri',
+                value: 'Zuri team',
+                url: 'https://training.zuri.team/',
+              },
+              {
+                id: 'books',
+                value: 'Zuri books',
+                subtext: 'The place to find books on design and coding.',
+                url: 'http://books.zuri.team/',
+              },
+              {
+                id: 'book__python',
+                value: 'Python books',
+                subtext: 'Learn python hassle-free today.',
+                url: 'https://books.zuri.team/python-for-beginners?ref_id=cyanhead',
+              },
+              {
+                id: 'pitch',
+                value: 'Background Check for Coders',
+                subtext: 'Our backround checks on coders are 🔥. Try it now.',
+                url: 'https://backgroundcheck.zuri.team/',
+                resetCase: true,
+              },
+              {
+                id: 'book__design',
+                value: 'Design books',
+                subtext: "Be the designer you've dreamed of, free of charge!",
+                url: 'https://books.zuri.team/design-rules',
+              },
+            ]}
+          />
+        </Section>
+        <Section flexDir="row" pad="24px">
+          <Link
+            href="https://slack.com/"
+            target="_blank"
+            rel="noreferrer"
+            mar="0 12px"
+          >
+            <PhotoWrap>
+              <Photo src={slack} alt="" />
+            </PhotoWrap>
+          </Link>
+          <Link
+            href="https://github.com/cyanhead"
+            target="_blank"
+            rel="noreferrer"
+            mar="0 12px"
+          >
+            <PhotoWrap>
+              <Photo src={github} alt="" />
+            </PhotoWrap>
+          </Link>
+        </Section>
+
+        {/* FOOTER SECTION */}
+        <Section flexDir="row" justCont="space-between" pad="84px 0 52px 0">
+          <PhotoWrap>
+            <Photo src={zuri} alt="" />
+          </PhotoWrap>
+          <Text fg="#667085">HNG Internship 9 Frontend Task</Text>
+          <PhotoWrap>
+            <Photo src={i4g} alt="" />
+          </PhotoWrap>
+        </Section>
+      </Wrap>
+    </Container>
+  );
+};
+
+export default HomePage;
