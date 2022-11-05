fcf5fbfa3b4cf7510f3c55e626b251f03161d25d
Bunmi Oye <bunmioye09@gmail.com>
2022-11-05 21:58:49 +0100

    feat: add routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import GlobalStyles from './global.style';
 import HomePage from './components/HomePage';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
 
-      <HomePage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/contact" element={<div>contact</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
